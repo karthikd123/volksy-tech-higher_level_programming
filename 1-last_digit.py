c49720dcc4138acda0b90811a6b7becdb9ad247f
karthikd123 <karthik123@brightcom.com>
2023-01-02 04:02:28 -0800

    TASK

--- 1-last_digit.py
+++ 1-last_digit.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = abs(number) %
+last = number % 10
 if number < 0:
-    digit = -digit
-    print("Last digit of {number:d} is {digit:n} and is ", end="")
-if digit> 5:
-    print("greater than 5")
-elif digit == 0:
-    print("0")
+    last = last - 10
+if last > 5:
+    print("Last digit of", number,"is",last,"and is greater than 5")
+elif last == 0:
+    print("Last digit of",number,"is",last,"and is 0")
 else:
-    print("less than 6 and not 0")
+    print("Last digit of",number,"is",last,"less than 6 and not 0")
