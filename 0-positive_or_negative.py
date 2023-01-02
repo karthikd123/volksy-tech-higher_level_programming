75206938da148bd7ad4ece33a6ff4da283e9a606
karthikd123 <karthik123@brightcom.com>
2023-01-02 00:45:56 -0800

    TASK

---
+++ 0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.radint(10, -10)
+if number > 0 :
+    print("{n is positive" .format(number))
+elif number == 0 :
+    print("{n is zero" .format(number))
+else
+    print("{n is negative" .format(number))
