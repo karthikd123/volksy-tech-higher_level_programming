2cd1f8e31f46f3b33f1dbb5d9f95e3bc30396237
karthikd123 <karthik123@brightcom.com>
2023-01-02 20:11:31 -0800

    T

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(10):
     for j in range(10):
         if i < j:
             if i == 8 and j == 9:
-                print (89)
+                print(89)
             else:
                 print("{}{}" .format(i, j), end=(", "))
