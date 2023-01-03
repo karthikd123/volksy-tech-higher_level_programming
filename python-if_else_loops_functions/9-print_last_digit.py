a7f9c700e87cb5025e4bb2033f911fe1f94a0b0e
karthikd123 <karthik123@brightcom.com>
2023-01-02 21:06:37 -0800

    T

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-def add(a,b):
-    return a + b
+def print_last_digit(number):
+    if number >= 0:
+        last_digit = number % 10
+    elif number < 0:
+        last_digit = (number * -1) % 10
+    print(last_digit, end='')
+    return last_digit
