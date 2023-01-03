4bc5475988f68d93c42db1aa94149b0abc31eec5
karthikd123 <karthik123@brightcom.com>
2023-01-03 01:14:20 -0800

    TASK

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,8 @@
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
+    1 arguments.
+    1:Hello
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
