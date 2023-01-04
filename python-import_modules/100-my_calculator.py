e4d26767d78951e2d11e6afb87c96c46507be287
karthikd123 <karthik123@brightcom.com>
2023-01-03 21:04:21 -0800

    T

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     """Handle basic arithemetic operations"""
     from calculator_1 import add, sub, mul, div
-    import sys
+    import calculatot_1
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
