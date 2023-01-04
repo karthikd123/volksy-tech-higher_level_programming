43a38e1242bd8e4807a11c3fa5c0f9aebb3b910d
karthikd123 <karthik123@brightcom.com>
2023-01-03 21:02:00 -0800

    T

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """Print the addition of all arguments."""
-    import sys
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    import hidden_4
+    for i in dir(hidden_4):
+        if "__" not in i:
+            print(i)
