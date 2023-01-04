9b147130cf3c4b3676ce109160e12c8bf168ffd9
karthikd123 <karthik123@brightcom.com>
2023-01-04 01:56:47 -0800

    T

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """Handle basic arithemetic operations."""
-    from calculator_1 import add, sub, mul, div
     import sys
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operator: +, -, * and /")
-        sys.exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argc[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, operator[sys.argv[2]](a, b)))
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+    if len(argv) != 4:
+        print("./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+        operator = argv[2]
+        n1 = int(argv[1])
+        n2 = int(argv[3])
+        if operator == "+":
+            print("{} + {} = {}".format(n1,n2,add(n1, n2)))
+        elif operator == "-":
+            print("{:d} - {:d} = {:d}".format(n1,n2,sub(n1, n2)))
+        elif operator == "*":
+            print("{:d} * {:d} = {:d}".format(n1,n2,mul(n1, n2)))
+        elif operator == "/":
+            print("{:d} / {:d} = {:d}".format(n1,n2,div(n1, n2)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+
