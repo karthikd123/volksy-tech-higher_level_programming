003543f4e070101a4d7644c832d66e74ec529da7
karthikd123 <karthik123@brightcom.com>
2023-01-05 05:01:40 -0800

    TASK12

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,3 +2,11 @@
 def delete_at(my_list=[], idx=0):
     if idx >= len(my_list) or idx < 0:
         return my_list
+    list = [1, 2, 3, 4, 5]
+idx = 5
+idx = 3
+idx = 4
+idx = 0
+new_list = delete_at(list, idx)
+print(new_list)
+print(list)
