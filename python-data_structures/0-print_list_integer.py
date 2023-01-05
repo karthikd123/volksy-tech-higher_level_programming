08cd75d3db15ce88bb0d2ee065c9669ade26635a
karthikd123 <karthik123@brightcom.com>
2023-01-04 21:03:32 -0800

    T

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    print_list_integer = __import__('0-print_list_integer').print_list_integer
-    my_list = [1, 2, 3, 4, 5]
-    print_list_integer(my_list, str.format())
+    for i in my_list:
+        print('{:d}'.format(i))
