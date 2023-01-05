00f0f0a8f689c459960f6f52e3f52274deb39652
karthikd123 <karthik123@brightcom.com>
2023-01-04 22:24:38 -0800

    T

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx <  number of element in my_list
+    if idx >  number of element in my_list
+    my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = replace_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
