0d302de495ab1adbae606c7151807a85c3b4fbbb
karthikd123 <karthik123@brightcom.com>
2023-01-09 01:10:17 -0800

    TASK2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    ret = 0
+    fot i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (ret)
