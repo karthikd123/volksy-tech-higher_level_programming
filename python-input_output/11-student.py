26c4bcd9bfbf2854e7c5b5dd1d55bc19b7586e68
karthikd123 <karthik123@brightcom.com>
2023-01-18 02:43:32 -0800

    TASK11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -26,10 +26,3 @@ class Student:
                 if elem in self.__dict__.keys():
                     temp[elem] = self.__dict__[elem]
             return temp
-
-    def reload_from_json(self, json):
-        """Replaces all items in `json`
-        """
-
-        for items in json.keys():
-            self.__dict__[items] = json[items]
