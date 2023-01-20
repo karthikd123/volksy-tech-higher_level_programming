c096e43b31d5518a947b028c4b08f9f732565226
karthikd123 <karthik123@brightcom.com>
2023-01-20 00:50:07 -0800

    TASK1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""base
+"""
+import json
+import csv
+
+
+class Base:
+    """Base of the other shapes
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
