a2e32e1d70e1a11ea2a7f375bcf3f1ea732c23ca
karthikd123 <karthik123@brightcom.com>
2023-01-23 02:37:24 -0800

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,53 +1,51 @@
 #!/usr/bin/python3
-"""square
-"""
+"""square"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Inherits from Rectangle
-    """
-
+    """Inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(width=size, height=size, x=x, y=y, id=id)
+        """Class constructor
+        Args:
+            size (int): Square parameter
+            x (int): Horizontal offset
+            y (int): Vertical offset
+            id (int): id attribute
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Creates a new string object from the given object
+        Returns: [Square] (<id>) <x>/<y> - <size>
+        """
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.size)
 
     @property
     def size(self):
+        """int: Assigns value to size"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """size needs to be an int
-        """
-
         self.width = value
-        self.height = value
-
-    def __str__(self):
-        """Returns formatted information display
-        """
-
-        return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
-                                             self.id, self.x, self.y,
-                                             self.width)
 
     def update(self, *args, **kwargs):
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                setattr(self, k, v)
-        elif len(args) != 0:
-            try:
-                self.id = args[0]
-                self.size = args[1]
-                self.x = args[2]
-                self.y = args[3]
-            except IndexError:
-                pass
+        """Assigns an argument to each attribute"""
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i, e in enumerate(args):
+                if i < len(attrs):
+                    setattr(self, attrs[i], e)
         else:
-            print()
+            for k, v in kwargs.items():
+                if hasattr(self, k):
+                    setattr(self, k, v)
 
     def to_dictionary(self):
-        """Returns a dict representation
-        """
-
-        return {'id': self.id, 'x': self.x, 'size': self.width, 'y': self.y}
+        """Returns: Dictionary representation of Square"""
+        my_dict = super().to_dictionary()
+        my_dict["size"] = my_dict["width"]
+        del my_dict["width"], my_dict["height"]
+        return my_dict
