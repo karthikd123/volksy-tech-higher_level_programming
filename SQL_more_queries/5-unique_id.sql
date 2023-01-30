8773bb3aaa4f5593646911925c049680e3303543
karthikd123 <karthik123@brightcom.com>
2023-01-29 23:23:39 -0800

    TASK5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,6 @@
--- creates the table unique_id
-CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1 UNIQUE,
-    name VARCHAR(256)
-);
+-- creates the table unique_id on your MySQL server.
+   -- unique_id description:
+      -- id INT - default value 1, must be unique
+      -- name VARCHAR(256)
+   -- The database name will be passed as an argument of the mysql command
+   -- If the table unique_id already exists, script should not fail
