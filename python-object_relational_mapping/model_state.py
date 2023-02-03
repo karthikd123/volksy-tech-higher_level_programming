e9489d631b1ce8669441422a6645765d6bebd205
karthikd123 <karthik123@brightcom.com>
2023-02-02 21:26:57 -0800

    TASKmodel

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
+# Inherits from Base Tips and links to the MySiQL table states.
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
