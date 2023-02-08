8041cce7c7d146a5a666b17a5928655d3692c87f
karthikd123 <karthik123@brightcom.com>
2023-02-07 22:25:19 -0800

    TASK10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
-"""Student
-"""
+''' task 10 '''
 
 
 class Student:
-    """Contains student data
-    """
-
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves dictionary of Student with conditions to filter
-        """
-
-        if attrs == None or type(attrs) != list:
+        ''' function '''
+        if attrs is None:
             return self.__dict__
         else:
             temp = {}
-            for elem in attrs:
-                if type(elem) != str:
-                    return self.__dict__
-                if elem in self.__dict__.keys():
-                    temp[elem] = self.__dict__[elem]
+            for i in attrs:
+                if i in self.__dict__:
+                    temp[i] = self.__dict__.get(i)
             return temp
