4ce1e5a1cc3aa7250e15e8a5f459032cd44f5f73
karthikd123 <karthik123@brightcom.com>
2023-02-08 00:18:03 -0800

    TASK12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,45 +1,14 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    # Fail checks, none, not a string
-    if not roman_string:
+def roman_to_int(st):
+    d1 = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sum = 0
+    if type(st) == str:
+        for i in range(len(st)):
+                val = d1[st[i]]
+                if i+1 < len(st) and d1[st[i+1]] > val:
+                    sum = sum - val
+                else:
+                    sum = sum + val
+        return sum
+    else:
         return 0
-    if not isinstance(roman_string, str):
-        return 0
-    if not roman_string.isupper():
-        return 0
-    # Dictionary for roman numerals
-    r_dict = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
-
-    result = 0
-    temp = list(roman_string)
-    # Concat 4 and 9s
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'V':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            idx += 1
-    # Search in dict for correct numbers and add
-    for k, v in r_dict.items():
-        for index in temp:
-            if index == k:
-                result += v
-    return result
