2fb5987722abff1ca10c615a67dc3db4475cc0c8
karthikd123 <karthikd@brightcomgroup.com>
2023-02-21 11:49:43 +0000

    TASK5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,17 +1,20 @@
 #!/usr/bin/node
-// script that gets the contents of a webpage and stores it in a file.
+// gets the contents of a webpage and stores it in a file.
 
-const url = process.argv[2];
-const file = process.argv[3];
-const req = require('request');
-const fileStream = require('fs');
+const request = require('request');
+const process = require('process');
+const fs = require('fs');
+const apiUrl = process.argv[2];
+const filePath = process.argv[3];
 
-req(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    fileStream.writeFile(file, body, 'utf-8', (error) => {
-      if (error) console.log(error);
+request.get(apiUrl, (err, response, body) => {
+  if (err === null) {
+    fs.writeFile(filePath, body, 'utf8', (fileError) => {
+      if (fileError !== null) {
+        console.log(fileError);
+      }
     });
+  } else {
+    console.log(err);
   }
 });
