c48753099a3dd030f8a5c9430578137ceff3b53b
karthikd123 <karthikd@brightcomgroup.com>
2023-02-21 11:52:49 +0000

    TASK

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-// reads and prints the content of a file.
+// read and print a file
+const fs = require('fs');
+const fileToRead = process.argv[2];
 
-const file = process.argv[2];
-const fileStream = require('fs');
-fileStream.readFile(file, 'utf-8', (error, data) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
+fs.readFile(fileToRead, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
   }
+  console.log(data);
 });
