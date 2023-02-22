5dc662d569a7bb7f85d4c7971668047379bf2e2f
karthikd123 <karthikd@brightcomgroup.com>
2023-02-22 06:57:38 +0000

    TASK101

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,17 +1,35 @@
 #!/usr/bin/node
-// script that gets the contents of a webpage and stores it in a file.
+// using Star Wars API, prints all `characters` of a given film, in order
+const request = require('request');
 
-const url = process.argv[2];
-const file = process.argv[3];
-const req = require('request');
-const fileStream = require('fs');
-
-req(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    fileStream.writeFile(file, body, 'utf-8', (error) => {
-      if (error) console.log(error);
+function getCharName (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(JSON.parse(body).name);
     });
+  });
+}
+
+async function charsInFilm (urlList) {
+  try {
+    let name;
+    for (const url of urlList) {
+      name = await getCharName(url);
+      console.log(name);
+    }
+  } catch (error) {
+    console.error(error);
   }
+}
+
+const filmsURL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(filmsURL, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const urlList = JSON.parse(body).characters;
+  charsInFilm(urlList);
 });
